c0aa3796896f29d744aad4d8d8093e49741b112b
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-03-12 16:27:49 +0300

    Completed check_graph and its tests

--- constructor/check_graph.py
+++ constructor/check_graph.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Set
 
 import numpy as np
 from exceptions import NegativeValueError
@@ -11,6 +11,7 @@ class CheckGraph():
     def is_not_gaps(bonds: Bondtype) -> bool: 
         """
         Detects gaps or invalid value in node enum,
+
         bonds-list can be empty, but node ids must
 
         i) be enumerated from 0 to max id without gaps,
@@ -35,66 +36,100 @@ class CheckGraph():
     def is_simple(bonds: Bondtype) -> bool:
         """
         Checks for the absence of multiple and cyclic edges
-        Likes that: (1, 2) and (2, 1), (0, 0), (1, 1) etc.
+
+        Likes that: (1, 2) and (2, 1) or (0, 0), (1, 1) etc.
 
         Args:
             bonds (List[Tuple[int,int]]): list of bonded node ids
 
         Returns:
-            bool: True it the graph is simple, False otherwise
+            bool: True if the graph is simple or empty, False otherwise
         """
         new_bonds: Bondtype = bonds + [(bond[1], bond[0]) for bond in bonds]
         return len(new_bonds) == len(set(new_bonds))
 
-    # def is_connected(bonds: List[List[int]]) -> bool:
-    #     label = dict()
-    #     for bead in bonds:
-    #         for i in range(len(bead)):
-    #             if not bead[i] in label:
-    #                 label[bead[i]] = 0
-    #     counter = 0
-    #     cluster = dict()
-    #     for bond in bonds:
-    #         if label[bond[0]] == label[bond[1]]:
-    #             if label[bond[0]] == 0:
-    #                 counter += 1
-    #                 label[bond[0]] = counter
-    #                 label[bond[1]] = counter
-    #                 cluster[counter] = [bond[0], bond[1]]
-    #         else:
-    #             if label[bond[0]] == 0 and label[bond[1]] != 0:
-    #                 label[bond[0]] = label[bond[1]]
-    #                 cluster[label[bond[0]]].append(bond[0])
-    #             elif label[bond[1]] == 0 and label[bond[0]] != 0:
-    #                 label[bond[1]] = label[bond[0]]
-    #                 cluster[label[bond[1]]].append(bond[1])
-    #             else:
-    #                 minimum = min(label[bond[1]], label[bond[0]])
-    #                 maximum = max(label[bond[1]], label[bond[0]])
-    #                 cluster[minimum] = cluster[minimum] + cluster[maximum]
-    #                 for clust in cluster[maximum]:
-    #                     label[clust] = minimum
-    #                 cluster.pop(maximum)
-    #     return len(cluster) == 1
+    @staticmethod
+    def is_connected(bonds: Bondtype) -> bool:
+        """
+        Full graph traversal to detect that it's connected  
 
-    # def is_directed(bonds: List[List[int]]) -> bool:
-    #     nodes = list(set([bond[0] for bond in bonds] + [bond[1]
-    #                  for bond in bonds]))
-    #     painted = set()
-    #     painted.add(bonds[0][0])
-    #     for bond in bonds:
-    #         if bond[0] in painted:
-    #             painted.add(bond[1])
-    #     return len(nodes) == len(list(painted))
+        (i.e. there is a path from any node to any node)
+
+        Args:
+            bonds (List[Tuple[int,int]]): list of bonded node ids
+
+        Returns:
+            bool: True if graph is connected, 
+                    False if it is disconnected or empty
+        """
+        label: Dict = dict()
+        for bond in bonds:
+            if not (bond[0] in label):
+                label[bond[0]] = 0
+            if not (bond[1] in label):
+                label[bond[1]] = 0    
+        counter: int = 0
+        cluster: Dict = dict()
+        minimum: int
+        maximum: int
+        for bond in bonds:
+            if label[bond[0]] == label[bond[1]]:
+                if label[bond[0]] == 0:
+                    counter += 1
+                    label[bond[0]] = counter
+                    label[bond[1]] = counter
+                    cluster[counter] = [bond[0], bond[1]]
+            else:
+                if label[bond[0]] == 0 and label[bond[1]] != 0:
+                    label[bond[0]] = label[bond[1]]
+                    cluster[label[bond[0]]].append(bond[0])
+                elif label[bond[1]] == 0 and label[bond[0]] != 0:
+                    label[bond[1]] = label[bond[0]]
+                    cluster[label[bond[1]]].append(bond[1])
+                else:
+                    minimum = min(label[bond[1]], label[bond[0]])
+                    maximum = max(label[bond[1]], label[bond[0]])
+                    cluster[minimum] = cluster[minimum] + cluster[maximum]
+                    for clust in cluster[maximum]:
+                        label[clust] = minimum
+                    cluster.pop(maximum)
+        return len(cluster) == 1
+
+    @staticmethod
+    def is_directed(bonds: Bondtype) -> bool:
+        """
+        Checks the possibility of constructing a <<directed>> graph
+
+        (the first id must be listed before the second in the bonds list)
+            Directed:     [(0, 1), (1, 2), (2, 3)]
+            Not directed: [(0, 1), (2, 1), (2, 3)]
+
+        Args:
+            bonds (List[Tuple[int,int]]): list of bonded node ids
+
+        Returns:
+            bool: True if graph is connected, 
+                    False if it is disconnected or empty
+        """
+        if bonds:
+            painted: Set = set()
+            painted.add(bonds[0][0])
+            for bond in bonds:
+                if bond[0] in painted:
+                    painted.add(bond[1])
+                else:
+                    return False
+            return True
+        return False
     
 if __name__ == '__main__':
     bonds0: List[Tuple[int,int]] = []
-    bonds1 = [(1, 2), (2, 1), (0, 5), (5, 4)]
-    bonds2 = [(1, 2), (3, 3), (1, 5), (5, 4)]
+    #bonds1 = [(1, 2), (2, 1), (0, 5), (5, 4)]
+    bonds2 = [(1, 2), (2, 3), (3, 5), (5, 5)]
     bonds3 = [(1, 2), (2, 3), (-1, 5), (5, 4)]
     print(CheckGraph.is_simple(bonds0))
-    print(CheckGraph.is_simple(bonds1))
-    print(CheckGraph.is_simple(bonds2))
+    print(CheckGraph.is_connected(bonds0))
+    print(CheckGraph.is_directed(bonds0))
     try:
         print(CheckGraph.is_not_gaps(bonds3))
     except NegativeValueError as err:
