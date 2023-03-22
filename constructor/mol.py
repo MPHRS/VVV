0c2d0b36d25bb755359e3b0c8fbaae49f1f21881
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2023-03-22 14:21:41 +0300

    Add brush mol

--- constructor/mol.py
+++ constructor/mol.py
@@ -1,16 +1,18 @@
-from constructor import MolGraph
 from bondset import Bondtype
+
+from constructor import MolGraph
 
 
 class Chain(MolGraph):
     """
     Chain molecular structure inherited from MolGraph
-    
+
     Args:
         MolGraph (_type_): see constructor.MolGraph
     Initial attribute:
         self.n (int): chain length
     """
+
     def __init__(self, n: int) -> None:
         if n < 1:
             raise ValueError(f'(n = {n}) set is invalid ')
@@ -19,17 +21,19 @@ class Chain(MolGraph):
         for i in range(self.n-1):
             bonds.append((i, i+1))
         super().__init__(bonds)
-        
+
+
 class Dendron(MolGraph):
     """
     Dendrit molecular structure inherited from MolGraph
-    
+
     Args:
         MolGraph (_type_): see constructor.MolGraph
     Initial attributes:
         self.n (int): spacer length
         self.g (int): number of generations in dendron
     """
+
     def __init__(self, n: int, g: int = 0, q: int = 2) -> None:
         if q <= 1 or g < 0 or n < 1:
             raise ValueError(f'(n = {n}, g = {g}, q = {q}) set is invalid ')
@@ -51,12 +55,66 @@ class Dendron(MolGraph):
                         bonds.append((id0, current_id))
                     else:
                         bonds.append((current_id-1, current_id))
-        super().__init__(bonds, sort = False)
-        #super().__init__(bonds)
-            
+        super().__init__(bonds, sort=False)
+        # super().__init__(bonds)
+
+
+class Brush(MolGraph):
+    """Molecular brush structure
+
+    Args:
+        MolGraph (_type_): see constructor.MolGraph
+    Initial attributes:
+        self.pd (int): polymerization degree
+        self.m (int): spacer length
+        self.n (int): side chain length
+        self.q (int): number of side chains grafting into one point
+        self.n_end_ch (int): number of ending chains
+        self.l_end_ch (int): length of ending chains
+    """
+
+    def __init__(self, pd: int, m: int, n: int, l_end_ch: int, q: int = 1, n_end_ch: int = 0) -> None:
+        if pd < 1 or m < 1 or l_end_ch < 0:
+            raise ValueError(
+                f'(pd = {pd}, m = {m}, l_end_ch = {l_end_ch}) set is invalid - the main core is empty')
+        if n < 0 or q < 0:
+            raise ValueError(
+                f'(n = {n}, q = {q}) set is invalid - length and number of side chains must be positive')
+        if n_end_ch > 2 or n_end_ch < 0:
+            raise ValueError(
+                f'(n_end_ch ={n_end_ch}) set is invalid - n_end_ch should be 0 or 1 or 2')
+        self.pd = pd
+        self.m = m
+        self.n = n
+        self.q = q
+        self.n_end_ch = n_end_ch
+        self.l_end_ch = l_end_ch
+        bonds: Bondtype = []
+
+        for i in range(self.n_end_ch * self.l_end_ch + self.pd * self.m - 1):
+            bonds.append((i, i+1))
+
+        n_curent = self.pd * self.m - 1 + self.n_end_ch * self.l_end_ch
+        for i in range(1, self.pd + 1):
+            if self.n_end_ch == 0:
+                num = (self.m * (i-1)) + (self.m // 2 + 1) - 1
+            else:
+                num = (self.m * (i-1)) + (self.m // 2 + 1) - 1 + self.l_end_ch
+            for t in range(self.q):
+                for j in range(self.n):
+                    n_curent += 1
+                    if j == 0:
+                        bonds.append((num, n_curent))
+                    else:
+                        bonds.append((n_curent-1, n_curent))
+
+        super().__init__(bonds, sort=False)
+
+
 if __name__ == '__main__':
-    chain = Chain(n=5)
-    print(chain.bonds)
-    dendron = Dendron(n=1, g = 2, q = 2)
-    print(dendron.bonds)    
-    
+    # chain = Chain(n=5)
+    # print(chain.bonds)
+    # dendron = Dendron(n=1, g = 2, q = 2)
+    # print(dendron.bonds)
+    brush = Brush(pd=2, m=3, n=3, q=1, n_end_ch=2, l_end_ch=2)
+    print(brush.bonds)
